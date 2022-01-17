f7cf8f7c85e770aef53d211c35ad29aa7011f335
beezzyy <adirbendavid17@gmail.com>
2022-01-16 21:42:29 -0500

    Fixing laravel

--- Laravel/storage/framework/views/dfba90245575129f7cf6c92d73a950ca973f7867.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php $__env->startSection('title', __('Server Error')); ?>
-<?php $__env->startSection('code', '500'); ?>
-<?php $__env->startSection('message', __('Server Error')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\yoni1\Desktop\Winter 2022\SOEN341\SOEN341 Project\SOEN341-Project\Laravel\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/500.blade.php ENDPATH**/ ?>
