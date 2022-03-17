83d5d2baf1b04d1283cb4b75a48ecb4ac50a4d52
alexye0810 <65577282+alexye0810@users.noreply.github.com>
2022-03-17 09:37:54 -0400

    Added Modal part

--- project341/resources/views/Categories_Laptop.blade.php
+++ project341/resources/views/Categories_Laptop.blade.php
@@ -1,3 +1,4 @@
+
   <!--================ Header start =================-->
   @include('layouts.header')
   <!--================ Header end =================-->
@@ -110,7 +111,7 @@
                   <div class="card-product__img">
                     <img class="card-img" src="img/product/Product1.jpg" alt="" width="100" height="100">
                     <ul class="card-product__imgOverlay">
-                      <li><button><i class="ti-search"></i></button></li>
+                      <li><button class="modal-button" href="#myModal1"><i class="ti-search"></i></button></li>
                       <li><button><i class="ti-shopping-cart"></i></button></li>
                       <li><button><i class="ti-heart"></i></button></li>
                     </ul>
@@ -121,13 +122,23 @@
                     <p class="card-product__price">$750.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal1" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product4.jpg" alt="" width="200" height="400">
+    			        <p>14 Inch Dell Computer ideal for home and school use.</p>
+    			        <p class="card-product__price">$750.00</p>
+  			        </div>
+		        </div>
               </div>
               <div class="col-md-6 col-lg-4">
                 <div class="card text-center card-product">
                   <div class="card-product__img">
                     <img class="card-img" src="img/product/Product2.jfif" alt="" width="100" height="100">
                     <ul class="card-product__imgOverlay">
-                      <li><button><i class="ti-search"></i></button></li>
+                      <li><button class="modal-button" href="#myModal2"><i class="ti-search"></i></button></li>
                       <li><button><i class="ti-shopping-cart"></i></button></li>
                       <li><button><i class="ti-heart"></i></button></li>
                     </ul>
@@ -138,13 +149,23 @@
                     <p class="card-product__price">$950.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal2" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product4.jpg" alt="" width="200" height="400">
+    			        <p>14 Inch Dell Work Computer with 2 years support included</p>
+    			        <p class="card-product__price">$950.00</p>
+  			        </div>
+		        </div>
               </div>
               <div class="col-md-6 col-lg-4">
                 <div class="card text-center card-product">
                   <div class="card-product__img">
                     <img class="card-img" src="img/product/Product3.jfif" alt="" width="100" height="100">
                     <ul class="card-product__imgOverlay">
-                      <li><button><i class="ti-search"></i></button></li>
+                      <li><button class="modal-button" href="#myModal3"><i class="ti-search"></i></button></li>
                       <li><button><i class="ti-shopping-cart"></i></button></li>
                       <li><button><i class="ti-heart"></i></button></li>
                     </ul>
@@ -155,13 +176,23 @@
                     <p class="card-product__price">$1250.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal3" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product3.jfif" alt="" width="200" height="400">
+    			        <p>14 Inch Dell Computer with QHD resolution</p>
+    			        <p class="card-product__price">$1250.00</p>
+  			        </div>
+		        </div>
               </div>
               <div class="col-md-6 col-lg-4">
                 <div class="card text-center card-product">
                   <div class="card-product__img">
                     <img class="card-img" src="img/product/Product4.jpg" alt="" width="100" height="100">
                     <ul class="card-product__imgOverlay">
-                      <li><button><i class="ti-search"></i></button></li>
+                      <li><button class="modal-button" href="#myModal4"><i class="ti-search"></i></button></li>
                       <li><button><i class="ti-shopping-cart"></i></button></li>
                       <li><button><i class="ti-heart"></i></button></li>
                     </ul>
@@ -172,13 +203,23 @@
                     <p class="card-product__price">$1250.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal4" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product4.jpg" alt="" width="200" height="400">
+    			        <p>14 Inch apple Computer that has a powerful M1 Chip</p>
+    			        <p class="card-product__price">$1250.00</p>
+  			        </div>
+		        </div>
               </div>
               <div class="col-md-6 col-lg-4">
                 <div class="card text-center card-product">
                   <div class="card-product__img">
                     <img class="card-img" src="img/product/Product5.jpg" alt="" width="100" height="100">
                     <ul class="card-product__imgOverlay">
-                      <li><button><i class="ti-search"></i></button></li>
+                      <li><button class="modal-button" href="#myModal5"><i class="ti-search"></i></button></li>
                       <li><button><i class="ti-shopping-cart"></i></button></li>
                       <li><button><i class="ti-heart"></i></button></li>
                     </ul>
@@ -189,13 +230,23 @@
                     <p class="card-product__price">$2000.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal5" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product5.jpg" alt="" width="200" height="400">
+    			        <p>21 Inch apple Computer that has a powerful M1 Chip</p>
+    			        <p class="card-product__price">$2000.00</p>
+  			        </div>
+		        </div>
               </div>
               <div class="col-md-6 col-lg-4">
                 <div class="card text-center card-product">
                   <div class="card-product__img">
                     <img class="card-img" src="img/product/Product6.jpg" alt="" width="100" height="100">
                     <ul class="card-product__imgOverlay">
-                      <li><button><i class="ti-search"></i></button></li>
+                      <li><button class="modal-button" href="#myModal6"><i class="ti-search"></i></button></li>
                       <li><button><i class="ti-shopping-cart"></i></button></li>
                       <li><button><i class="ti-heart"></i></button></li>
                     </ul>
@@ -206,6 +257,16 @@
                     <p class="card-product__price">$1750.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal6" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product6.jpg" alt="" width="200" height="400">
+    			        <p>16 Inch apple Computer that has a powerful M1 Chip</p>
+    			        <p class="card-product__price">$1750.00</p>
+  			        </div>
+		        </div>
               </div>
               <div class="col-md-6 col-lg-4">
                 <div class="card text-center card-product">
@@ -223,13 +284,23 @@
                     <p class="card-product__price">$1250.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal7" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product7.jpg" alt="" width="200" height="400">
+    			        <p>14 Inch Samsung Flex 360 Pro WHQD+ resolution, I9 32 gb Ram</p>
+    			        <p class="card-product__price">$1250.00</p>
+  			        </div>
+		        </div>
               </div>
               <div class="col-md-6 col-lg-4">
                 <div class="card text-center card-product">
                   <div class="card-product__img">
                     <img class="card-img" src="img/product/Product8.jpg" alt="" width="100" height="100">
                     <ul class="card-product__imgOverlay">
-                      <li><button><i class="ti-search"></i></button></li>
+                      <li><button class="modal-button" href="#myModal8"><i class="ti-search"></i></button></li>
                       <li><button><i class="ti-shopping-cart"></i></button></li>
                       <li><button><i class="ti-heart"></i></button></li>
                     </ul>
@@ -240,13 +311,23 @@
                     <p class="card-product__price">$750.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal8" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product8.jpg" alt="" width="200" height="400">
+    			        <p>14 Inch Samsung Flex 360 Laptop I7</p>
+    			        <p class="card-product__price">$750.00</p>
+  			        </div>
+		        </div>
               </div>
               <div class="col-md-6 col-lg-4">
                 <div class="card text-center card-product">
                   <div class="card-product__img">
                     <img class="card-img" src="img/product/Product9.png" alt="" width="100" height="100">
                     <ul class="card-product__imgOverlay">
-                      <li><button><i class="ti-search"></i></button></li>
+                      <li><button class="modal-button" href="#myModal9"><i class="ti-search"></i></button></li>
                       <li><button><i class="ti-shopping-cart"></i></button></li>
                       <li><button><i class="ti-heart"></i></button></li>
                     </ul>
@@ -257,13 +338,23 @@
                     <p class="card-product__price">$550.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal9" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product9.png" alt="" width="200" height="400">
+    			        <p>14 Inch Asus E676. New 2022 Model Ryzen 6700</p>
+    			        <p class="card-product__price">$550.00</p>
+  			        </div>
+		        </div>
               </div>
               <div class="col-md-6 col-lg-4">
                 <div class="card text-center card-product">
                   <div class="card-product__img">
                     <img class="card-img" src="img/product/Product10.png" alt="" width="100" height="100">
                     <ul class="card-product__imgOverlay">
-                      <li><button><i class="ti-search"></i></button></li>
+                      <li><button class="modal-button" href="#myModal10"><i class="ti-search"></i></button></li>
                       <li><button><i class="ti-shopping-cart"></i></button></li>
                       <li><button><i class="ti-heart"></i></button></li>
                     </ul>
@@ -274,13 +365,23 @@
                     <p class="card-product__price">$750.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal10" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product10.png" alt="" width="200" height="400">
+    			        <p>15 Inch Asus Computer. Ryzen 5 8 gb 256 gb</p>
+    			        <p class="card-product__price">$750.00</p>
+  			        </div>
+		        </div>
               </div>
               <div class="col-md-6 col-lg-4">
                 <div class="card text-center card-product">
                   <div class="card-product__img">
                     <img class="card-img" src="img/product/Product11.png" alt="" width="100" height="100">
                     <ul class="card-product__imgOverlay">
-                      <li><button><i class="ti-search"></i></button></li>
+                      <li><button class="modal-button" href="#myModal11"><i class="ti-search"></i></button></li>
                       <li><button><i class="ti-shopping-cart"></i></button></li>
                       <li><button><i class="ti-heart"></i></button></li>
                     </ul>
@@ -291,13 +392,23 @@
                     <p class="card-product__price">$1250.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal11" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product11.png" alt="" width="200" height="400">
+    			        <p>14 Inch Suurface Laptop by Microsoft. Mouse not included.</p>
+    			        <p class="card-product__price">$1250.00</p>
+  			        </div>
+		        </div>
               </div>
               <div class="col-md-6 col-lg-4">
                 <div class="card text-center card-product">
                   <div class="card-product__img">
                     <img class="card-img" src="img/product/Product12.png" alt="" width="100" height="100">
                     <ul class="card-product__imgOverlay">
-                      <li><button><i class="ti-search"></i></button></li>
+                      <li><button class="modal-button" href="#myModal12"><i class="ti-search"></i></button></li>
                       <li><button><i class="ti-shopping-cart"></i></button></li>
                       <li><button><i class="ti-heart"></i></button></li>
                     </ul>
@@ -308,6 +419,16 @@
                     <p class="card-product__price">$1250.00</p>
                   </div>
                 </div>
+                <!-- The Modal -->
+		        <div id="myModal12" class="modal">
+  		            <!-- Modal content -->
+  			        <div class="modal-content">
+    			        <span class="close">&times;</span>
+   		 	            <img class="center" src="img/product/Product12.png" alt="" width="200" height="400">
+    			        <p>14 Inch Microsoft Suface Pro. Intel I9  16gb ram 512 Gb</p>
+    			        <p class="card-product__price">$1250.00</p>
+  			        </div>
+		        </div>
               </div>
             </div>
           </section>
@@ -449,6 +570,11 @@
   <script src="vendors/jquery.ajaxchimp.min.js"></script>
   <script src="vendors/mail-script.js"></script>
   <script src="js/main.js"></script>
+<<<<<<< Michael
   </body>
 
   </html>
+=======
+  <script src="js/addedjs.js"></script>
+</body>
+</html>
