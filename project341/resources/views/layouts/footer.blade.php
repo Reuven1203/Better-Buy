a169d0afd88daac776a93fc514a467ddc6c98400
Michael Roubeiz <michaelroubeiz@gmail.com>
2022-03-17 21:49:35 -0400

    Add footer page to cleanup code

---
+++ project341/resources/views/layouts/footer.blade.php
@@ -0,0 +1,88 @@
+ <!--================ Start footer Area  =================-->
+ <footer>
+    <div class="footer-area">
+      <div class="container">
+        <div class="row section_gap">
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="single-footer-widget tp_widgets">
+              <h4 class="footer_title large_title">Our Mission</h4>
+              <p>
+               Provide BETTER electronic items!
+              </p>
+            </div>
+          </div>
+          <div class="offset-lg-1 col-lg-2 col-md-6 col-sm-6">
+            <div class="single-footer-widget tp_widgets">
+              <h4 class="footer_title">Quick Links</h4>
+              <ul class="list">
+                <li><a href="index">Home</a></li>
+                <li><a href="category">Shop</a></li>
+                <!-- <li><a href="#">Blog</a></li> -->
+                <li><a href="category">Product</a></li>
+                <!-- <li><a href="#">Brand</a></li> -->
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <!-- <div class="col-lg-2 col-md-6 col-sm-6">
+            <div class="single-footer-widget instafeed">
+              <h4 class="footer_title">Gallery</h4>
+              <ul class="list instafeed d-flex flex-wrap">
+                <li><img src="img/gallery/r1.jpg" alt=""></li>
+                <li><img src="img/gallery/r2.jpg" alt=""></li>
+                <li><img src="img/gallery/r3.jpg" alt=""></li>
+                <li><img src="img/gallery/r5.jpg" alt=""></li>
+                <li><img src="img/gallery/r7.jpg" alt=""></li>
+                <li><img src="img/gallery/r8.jpg" alt=""></li>
+              </ul>
+            </div>
+          </div> -->
+          <div class="offset-lg-1 col-lg-3 col-md-6 col-sm-6">
+            <div class="single-footer-widget tp_widgets">
+              <h4 class="footer_title">Contact Us</h4>
+              <div class="ml-40">
+                <p class="sm-head">
+                  <span class="fa fa-location-arrow"></span>
+                  Head Office
+                </p>
+                <p>123, Main Street, Your City</p>
+
+                <p class="sm-head">
+                  <span class="fa fa-phone"></span>
+                  Phone Number
+                </p>
+                <p>
+                  +123 456 7890 <br>
+                  +123 456 7890
+                </p>
+
+                <p class="sm-head">
+                  <span class="fa fa-envelope"></span>
+                  Email
+                </p>
+                <p>
+                  free@infoexample.com <br>
+                  www.infoexample.com
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row d-flex">
+          <p class="col-lg-12 footer-text text-center">
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>
+              document.write(new Date().getFullYear());
+            </script> 
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--================ End footer Area  =================-->
