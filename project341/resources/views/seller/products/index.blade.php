322fd88b7c059ae07a45a8a8b365a17ab80078d6
Reuven1203 <reuven1203@gmail.com>
2022-03-20 22:49:17 -0400

    delete button

--- project341/resources/views/seller/products/index.blade.php
+++ project341/resources/views/seller/products/index.blade.php
@@ -38,8 +38,11 @@ use App\Http\Controllers\Admin\ProductController;
                                         <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                             Stock
                                         </th>
-                                        <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
+                                        <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                             Price
+                                        </th>
+                                        <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
+                                            Commands
                                         </th>
                                     </tr>
                                 </thead>
@@ -65,6 +68,14 @@ use App\Http\Controllers\Admin\ProductController;
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                                             {{ $product->price }}
                                         </td>
+                                        <td>
+                                            <form class="inline-block" action="{{ route('seller.products.destroy', $product->id) }}" method="POST" onsubmit="return confirm('Are you sure?');">
+                                                <input type="hidden" name="_method" value="DELETE">
+                                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                <input type="submit" class="text-red-600 hover:text-red-900 mb-2 mr-2" value="Delete">
+                                            </form>
+                                        </td>
+
 
                                     </tr>
                                     @endforeach
