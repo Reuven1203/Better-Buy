870d274dc01048984953cfb3985a6ac6409f6218
beezzyy <adirbendavid17@gmail.com>
2022-03-21 15:49:42 -0400

    Update index.blade.php

--- project341/resources/views/seller/products/index.blade.php
+++ project341/resources/views/seller/products/index.blade.php
@@ -71,7 +71,7 @@ use App\Http\Controllers\Admin\ProductController;
                                             <form class="inline-block" action="{{ route('seller.products.edit', $product->id) }}" method="get">
                                                 <input type="hidden" name="_method" value="Edit">
                                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                <input type="submit" class="text-blue-600 hover:text-blue-900 mb-2 mr-2" style="margin-left:60px" value="Edit">
+                                                <input type="submit" class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2" style="margin-left:60px" value="Edit">
                                             </form>
                                             <form class="inline-block" action="{{ route('seller.products.destroy', $product->id) }}" method="POST" onsubmit="return confirm('Are you sure?');">
 
