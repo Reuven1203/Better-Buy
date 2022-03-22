0ba1c7f114be560f516745dea55e83e64a126c22
Reuven1203 <reuven1203@gmail.com>
2022-03-22 06:42:17 -0400

    fixed admin product view

--- project341/resources/views/seller/products/index.blade.php
+++ project341/resources/views/seller/products/index.blade.php
@@ -1,3 +1,9 @@
+<?php
+
+use App\Models\Product;
+
+$allProducts = Product::all();
+?>
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -41,7 +47,9 @@
                                     </tr>
                                 </thead>
                                 <tbody class="bg-white divide-y divide-gray-200">
+                                    @if (auth()->user()->role_id != "Admin" && auth()->user()->role_id == "Seller")
                                     @foreach ($user->products as $product)
+
                                     <tr>
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                                             {{ $product->id }}
@@ -82,6 +90,51 @@
 
                                     </tr>
                                     @endforeach
+                                    @endif
+                                    @if(auth()->user()->role_id == 'Admin')
+                                    @foreach ($allProducts as $product)
+
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $product->id }}
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $product->name }}
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $product->brand }}
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $product->stock }}
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $product->price }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $product->category }}
+                                        </td>
+                                        <td>
+                                            <form class="inline-block" action="{{ route('seller.products.edit', $product->id) }}" method="get">
+                                                <input type="hidden" name="_method" value="Edit">
+                                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                <input type="submit" class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2" style="margin-left:60px" value="Edit">
+                                            </form>
+                                            <form class="inline-block" action="{{ route('seller.products.destroy', $product->id) }}" method="POST" onsubmit="return confirm('Are you sure?');">
+
+                                                <input type="hidden" name="_method" value="DELETE">
+                                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                <input type="submit" class="text-red-600 hover:text-red-900 mb-2 mr-2" style="margin-left:60px" value="Delete">
+                                            </form>
+                                        </td>
+
+
+                                    </tr>
+                                    @endforeach
+                                    @endif
                                 </tbody>
                             </table>
                         </div>
