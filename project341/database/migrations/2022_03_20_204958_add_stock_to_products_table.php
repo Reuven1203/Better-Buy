e454d8467bba4782cc76040cdad59993c314f729
Reuven1203 <reuven1203@gmail.com>
2022-03-24 19:35:11 -0400

    fixed migration

--- project341/database/migrations/2022_03_20_204958_add_stock_to_products_table.php
+++ project341/database/migrations/2022_03_20_204958_add_stock_to_products_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->double('stock')->nullable();
         });
     }
 
