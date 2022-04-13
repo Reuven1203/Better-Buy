c55f40c30732350a0341d85e97e60ebd979535ef
williamau8985 <36796101+williamau8985@users.noreply.github.com>
2022-04-12 21:22:13 -0400

    Added the date field and removed unnecessary columns

--- project341/resources/views/client/orders/index.blade.php
+++ project341/resources/views/client/orders/index.blade.php
@@ -35,6 +35,9 @@ $allProducts = Product::all();
                                         <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                             Price
                                         </th>
+                                        <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Date
+                                        </th>
             
                                         <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
                                             Commands
@@ -42,33 +45,24 @@ $allProducts = Product::all();
                                     </tr>
                                 </thead>
                                 <tbody class="bg-white divide-y divide-gray-200">
-                                    @if (auth()->user()->role_id != "Admin" && auth()->user()->role_id == "Seller")
-                                    @foreach ($user->products as $product)
+                                    @if (auth()->user()->role_id != "Admin" && auth()->user()->role_id == "Client")
+                                    @foreach ($user->order as $order)
                                     <tr>
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                            {{ $product->id }}
+                                            {{ $order->id }}
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                                             <img src="/storage/{{$product->image}}" alt="">
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                            {{ $product->name }}
-                                        </td>
-
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                            {{ $product->brand }}
-                                        </td>
-
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                            {{ $product->stock }}
-                                        </td>
-
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                            {{ $product->price }}
+                                            {{ $order->name }}
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                            {{ $product->category }}
+                                            {{ $order->price }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $order->date }}
                                         </td>
                                         <td>
                                             <form class="inline-block" action="{{ route('seller.products.edit', $product->id) }}" method="get">
@@ -114,12 +108,10 @@ $allProducts = Product::all();
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                                             {{ $order->price }}
                                         </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $order->date }}
+                                        </td>
                                         <td>
-                                            <form class="inline-block" action="{{ route('seller.products.edit', $product->id) }}" method="get">
-                                                <input type="hidden" name="_method" value="Edit">
-                                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                <input type="submit" class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2" style="margin-left:60px" value="Edit">
-                                            </form>
                                             <form class="inline-block" action="{{ route('seller.products.destroy', $product->id) }}" method="POST" onsubmit="return confirm('Are you sure?');">
 
                                                 <input type="hidden" name="_method" value="DELETE">
