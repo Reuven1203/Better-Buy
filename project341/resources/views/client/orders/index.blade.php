82199add98ed1570721b640305c83d759be0226e
williamau8985 <36796101+williamau8985@users.noreply.github.com>
2022-04-12 20:42:09 -0400

    Added the Layout for client Orders

--- project341/resources/views/client/orders/index.blade.php
+++ project341/resources/views/client/orders/index.blade.php
@@ -1,13 +1,23 @@
+<?php
+
+use App\Models\Product;
+
+$allProducts = Product::all();
+?>
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Orders List') }}
+            Products List
         </h2>
     </x-slot>
 
     <div>
-        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            Accessible only for Clients.
+        <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="block mb-8">
+                <a href="{{ route('seller.products.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" style="margin-left:35px">Add Product</a>
+            </div>
+            
+
         </div>
     </div>
 </x-app-layout>
