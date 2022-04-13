33a6ccb31277a5edba50265072fdf19f428361ee
williamau8985 <36796101+williamau8985@users.noreply.github.com>
2022-04-12 20:50:48 -0400

    Adjustments to conform to intended layout

--- project341/resources/views/client/orders/index.blade.php
+++ project341/resources/views/client/orders/index.blade.php
@@ -7,15 +7,13 @@ $allProducts = Product::all();
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Products List
+            Orders List
         </h2>
     </x-slot>
 
     <div>
         <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
-            <div class="block mb-8">
-                <a href="{{ route('seller.products.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" style="margin-left:35px">Add Product</a>
-            </div>
+            
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
