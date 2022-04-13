b1332a9f4ab5aed7bc7dd9f0fe111432caa9e74b
Michael Roubeiz <michaelroubeiz@gmail.com>
2022-04-12 20:59:01 -0400

    Formatted file

--- project341/resources/views/checkout.blade.php
+++ project341/resources/views/checkout.blade.php
@@ -60,48 +60,52 @@ foreach (\Cart::getContent() as $item) {
             color: #fff;
         }
 
-* {
-  box-sizing: border-box;
-}
+        * {
+            box-sizing: border-box;
+        }
 
-.row {
-  display: -ms-flexbox; /* IE10 */
-  display: flex;
-  -ms-flex-wrap: wrap; /* IE10 */
-  flex-wrap: wrap;
-  margin: 0 -16px;
-}
+        .row {
+            display: -ms-flexbox;
+            /* IE10 */
+            display: flex;
+            -ms-flex-wrap: wrap;
+            /* IE10 */
+            flex-wrap: wrap;
+            margin: 0 -16px;
+        }
 
-.col-25 {
-  -ms-flex: 25%; /* IE10 */
-  flex: 25%;
-}
+        .col-25 {
+            -ms-flex: 25%;
+            /* IE10 */
+            flex: 25%;
+        }
 
-.col-50 {
-  -ms-flex: 50%; /* IE10 */
-  flex: 50%;
-}
+        .col-50 {
+            -ms-flex: 50%;
+            /* IE10 */
+            flex: 50%;
+        }
 
-.col-75 {
-  -ms-flex: 75%; /* IE10 */
-  flex: 75%;
-}
+        .col-75 {
+            -ms-flex: 75%;
+            /* IE10 */
+            flex: 75%;
+        }
 
-.col-25,
-.col-50,
-.col-75 {
-  padding: 0 16px;
-}
+        .col-25,
+        .col-50,
+        .col-75 {
+            padding: 0 16px;
+        }
 
 
-input[type=text] {
-  width: 100%;
-  margin-bottom: 20px;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-}
-
+        input[type=text] {
+            width: 100%;
+            margin-bottom: 20px;
+            padding: 12px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+        }
     </style>
 </head>
 
@@ -188,7 +192,7 @@ input[type=text] {
                                 <th scope="col">Total</th>
                             </tr>
                         </thead>
-                        <?php $cartItems=\Cart::session($userId)->getContent()?>
+                        <?php $cartItems = \Cart::session($userId)->getContent() ?>
                         @foreach ($cartItems as $item)
                         @if (Product::find($item->id) != null)
 
@@ -232,7 +236,7 @@ input[type=text] {
                                 <td>
 
                                 </td>
-                            
+
                             </tr>
                             <tr>
                                 <td>
@@ -262,71 +266,71 @@ input[type=text] {
                                     <p>Free Shipping</p>
                                 </td>
                             </tr>
-                                        <tr>                                         
-  <div class="col-75">
-    <div class="container">
-      <form action="/action_page.php">
-      
-        <div class="row">
-          <div class="col-50">
-            <h5>Billing Address</h5>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
-            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="john@example.com">
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input type="text" id="city" name="city" placeholder="New York">
+                            <tr>
+                                <div class="col-75">
+                                    <div class="container">
+                                        <form action="/action_page.php">
 
-            <div class="row">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input type="text" id="state" name="state" placeholder="NY">
-              </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="10001">
-              </div>
-            </div>
-          </div>
+                                            <div class="row">
+                                                <div class="col-50">
+                                                    <h5>Billing Address</h5>
+                                                    <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+                                                    <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
+                                                    <label for="email"><i class="fa fa-envelope"></i> Email</label>
+                                                    <input type="text" id="email" name="email" placeholder="john@example.com">
+                                                    <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+                                                    <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
+                                                    <label for="city"><i class="fa fa-institution"></i> City</label>
+                                                    <input type="text" id="city" name="city" placeholder="New York">
 
-          <div class="col-50">
-            <h5>Payment</h5>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa" style="color:navy;"></i>
-              <i class="fa fa-cc-amex" style="color:blue;"></i>
-              <i class="fa fa-cc-mastercard" style="color:red;"></i>
-              <i class="fa fa-cc-discover" style="color:orange;"></i>
-            </div>
-            <label for="cname">Name on Card</label>
-            <input type="text" id="cname" name="cardname" placeholder="John More Doe">
-            <label for="ccnum">Credit card number</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
-            <label for="expmonth">Exp Month</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="September">
-            <div class="row">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018">
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="352">
-              </div>
-            </div>
-          </div>
-          
-        </div>
-        <label>
-          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
-        </label>
-      </form>
-    </div>
-  </div>
-</tr>
-                                
+                                                    <div class="row">
+                                                        <div class="col-50">
+                                                            <label for="state">State</label>
+                                                            <input type="text" id="state" name="state" placeholder="NY">
+                                                        </div>
+                                                        <div class="col-50">
+                                                            <label for="zip">Zip</label>
+                                                            <input type="text" id="zip" name="zip" placeholder="10001">
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-50">
+                                                    <h5>Payment</h5>
+                                                    <label for="fname">Accepted Cards</label>
+                                                    <div class="icon-container">
+                                                        <i class="fa fa-cc-visa" style="color:navy;"></i>
+                                                        <i class="fa fa-cc-amex" style="color:blue;"></i>
+                                                        <i class="fa fa-cc-mastercard" style="color:red;"></i>
+                                                        <i class="fa fa-cc-discover" style="color:orange;"></i>
+                                                    </div>
+                                                    <label for="cname">Name on Card</label>
+                                                    <input type="text" id="cname" name="cardname" placeholder="John More Doe">
+                                                    <label for="ccnum">Credit card number</label>
+                                                    <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+                                                    <label for="expmonth">Exp Month</label>
+                                                    <input type="text" id="expmonth" name="expmonth" placeholder="September">
+                                                    <div class="row">
+                                                        <div class="col-50">
+                                                            <label for="expyear">Exp Year</label>
+                                                            <input type="text" id="expyear" name="expyear" placeholder="2018">
+                                                        </div>
+                                                        <div class="col-50">
+                                                            <label for="cvv">CVV</label>
+                                                            <input type="text" id="cvv" name="cvv" placeholder="352">
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                            <label>
+                                                <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
+                                            </label>
+                                        </form>
+                                    </div>
+                                </div>
+                            </tr>
+
                             <tr class="out_button_area">
                                 <td class="d-none-l">
 
