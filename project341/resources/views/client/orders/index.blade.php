90f19f666930c7638cf36f1457ce16c9144a1d8c
williamau8985 <36796101+williamau8985@users.noreply.github.com>
2022-04-12 20:46:25 -0400

    Added formatting for table for the layout for orders

--- project341/resources/views/client/orders/index.blade.php
+++ project341/resources/views/client/orders/index.blade.php
@@ -16,7 +16,15 @@ $allProducts = Product::all();
             <div class="block mb-8">
                 <a href="{{ route('seller.products.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" style="margin-left:35px">Add Product</a>
             </div>
-            
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                          
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
